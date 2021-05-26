73eff7b054c7435ebc0464cacf86f636dc53fdd4
lenny-louis <louislennyy@gmail.com>
2021-05-26 16:42:06 +0200

    third push

--- src/userphone.php
+++ src/userphone.php
@@ -3,24 +3,24 @@ final class User
 {
     private $userphone;
 
-    private function __construct(string $userphone)
+    private function __construct(int $userphone)
     {
         $this->ensureIsValidEmailUserPhone($userphone);
 
         $this->userphone = $userphone;
     }
 
-    public static function fromString(string $userphone): self
+    public static function fromString(int $userphone): self
     {
         return new self($userphone);
     }
 
-    public function __toString(): string
+    public function __toInt(): int
     {
         return $this->userphone;
     }
 
-    private function ensureIsValidUserPhone(string $userphone): void
+    private function ensureIsValidUserPhone(int $userphone): void
     {
         if (!filter_var($user, FILTER_VALIDATE_INT)) {
             throw new InvalidArgumentException(
